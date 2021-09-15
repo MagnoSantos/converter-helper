5a41a102992dc7b6efea3426fee37ca3539ab2c2
Magno Juliano Goncalves Santos <magno.santos@neon.com.br>
2021-09-15 15:41:15 -0300

    Alteração do program cs para teste da conversão

--- src/Converter.Exemplos/Converter.Exemplos/Program.cs
+++ src/Converter.Exemplos/Converter.Exemplos/Program.cs
@@ -12,7 +12,21 @@ namespace Converter.Exemplos
         static Task Main(string[] args)
         {
             using IHost host = CreateHostBuilder(args).Build();
-            
+
+            //Conversão pelas abstracoes IOneWayConverter ou ITwoWayConverter
+            using var serviceScope = host.Services.CreateScope();
+            var provider = serviceScope.ServiceProvider;
+
+            var customerDto = new CustomerDto
+            {
+                Name = "Teste", 
+                Cpf = "12345678900",
+                Email = "teste@neon.com.br",
+                LastName = "LastTest"
+            };
+            var converter = provider.GetRequiredService<IOneWayConverter<CustomerDto, CustomerDataBase>>();
+            var result = converter.Convert(customerDto);
+
             return host.RunAsync();
         }
 
